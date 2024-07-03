00b920018d9607b8672c5208345c18a7e0c35eb3
Nikutsuki <nikutsuke@gmail.com>
2024-07-03 19:30:13 +0200

    womp

--- WIG-Verification/Plugin/src/main/java/org/wig/verification/Verification.java
+++ WIG-Verification/Plugin/src/main/java/org/wig/verification/Verification.java
@@ -1,63 +1,41 @@
-package org.wig.verification;
-
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
+package org.wigverification.wigverification;
+import com.sun.net.httpserver.HttpServer;
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.awt.*;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
+import java.net.InetSocketAddress;
 
-public final class Verification extends JavaPlugin implements Listener {
-    private static final HttpClient client = HttpClient.newHttpClient();
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public final class WIG_Verification extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(6969), 0);
+            server.createContext("/add_whitelist", new WhitelistHandler());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            Bukkit.getLogger().info(e.getMessage());
+        }
     }
     @Override
     public void onDisable() {
 
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://127.0.0.1:3000/verify_user"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString("{\"username\":\"" + event.getPlayer().getName() + "\"}"))
-                .timeout(Duration.of(30, ChronoUnit.SECONDS))
-                .build();
-        try {
-            HttpResponse<String> httpResponse = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            JSONParser parser = new JSONParser();
-            JSONObject jsonObject;
+    static class WhitelistHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) {
             try {
-                jsonObject = (JSONObject) parser.parse(httpResponse.body());
-                boolean verified = (Boolean) jsonObject.get("verified");
-                if(verified) {
-                    event.getPlayer().sendMessage("Witaj na serwerze WIG!");
-                } else {
-                    Component kickMessage = Component.text("Nie jesteś zweryfikowany.").color(TextColor.color(0xFFFFFF));
-                    event.getPlayer().kick(kickMessage);
-                }
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
+                String nickname = t.getRequestURI().getQuery().split("=")[1];
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "wl add" + nickname);
+            } catch (Exception e) {
+                Bukkit.getLogger().info(e.getMessage());
             }
-        } catch(IOException | InterruptedException e) {
-            Bukkit.getLogger().info(e.getMessage());
         }
     }
 }
