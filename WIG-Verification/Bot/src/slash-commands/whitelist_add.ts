34ed0d70bdc96aef301efc2f2ec85c4ae45a58ef
Nikutsuki <nikutsuke@gmail.com>
2024-07-04 23:29:14 +0200

    owo

--- WIG-Verification/Bot/src/slash-commands/whitelist_add.ts
+++ WIG-Verification/Bot/src/slash-commands/whitelist_add.ts
@@ -5,18 +5,9 @@ const Command = {
     data: new SlashCommandBuilder()
         .setName('whitelist_add')
         .setDescription('Add a user to the whitelist.')
-        .addStringOption(option => option.setName('nickname').setDescription('User nickname.').setRequired(true)),
+        .addStringOption(option => option.setName('nickname').setDescription('User nickname.').setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
     execute: async (client: Client, interaction: CommandInteraction) => {
-        const member_roles = interaction.member?.roles as Array<Snowflake>;
-
-        if(member_roles.find(role => role === '1176280926112723025') === undefined) {
-            await interaction.reply({
-                content: `You do not have permission to use this command.`,
-                ephemeral: true
-            });
-            return;
-        }
-
         sendNickname(interaction.options.get('nickname')?.value as string);
         console.log(interaction.options.get('nickname')?.value as string);
 
