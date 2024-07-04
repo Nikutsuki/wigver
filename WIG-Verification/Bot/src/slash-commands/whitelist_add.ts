291b2fef01fccf9ada92a3d7df8d558701145ea0
Nikutsuki <nikutsuke@gmail.com>
2024-07-04 23:18:20 +0200

    roles uwu

--- WIG-Verification/Bot/src/slash-commands/whitelist_add.ts
+++ WIG-Verification/Bot/src/slash-commands/whitelist_add.ts
@@ -1,13 +1,22 @@
-import { Client, CommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { Client, CommandInteraction, PermissionFlagsBits, PermissionsBitField, Role, SlashCommandBuilder, Snowflake } from "discord.js";
 import sendNickname from "../utils/plugin";
 
 const Command = {
     data: new SlashCommandBuilder()
         .setName('whitelist_add')
         .setDescription('Add a user to the whitelist.')
-        .addStringOption(option => option.setName('nickname').setDescription('User nickname.').setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+        .addStringOption(option => option.setName('nickname').setDescription('User nickname.').setRequired(true)),
     execute: async (client: Client, interaction: CommandInteraction) => {
+        const member_roles = interaction.member?.roles as Array<Snowflake>;
+
+        if(member_roles.find(role => role === '1176280926112723025') === undefined) {
+            await interaction.reply({
+                content: `You do not have permission to use this command.`,
+                ephemeral: true
+            });
+            return;
+        }
+
         sendNickname(interaction.options.get('nickname')?.value as string);
         console.log(interaction.options.get('nickname')?.value as string);
 
